b7ad2db06d02a8a6c6561987b7c25b1353fd0f57
DEVOLO\Dimitar.Jilanov <djilanov@gmail.com>
2018-12-03 00:14:40 +0200

    Move mongo address to env

--- backend/bin/config/index.js
+++ backend/bin/config/index.js
@@ -4,6 +4,6 @@ module.exports = {
     port: process.env.PORT || 3200,
   },
   database: {
-    dbAddress: process.env.dockerMongoUrl || 'mongodb://admin:admin1234@ds063158.mlab.com:63158/node-react'
+    dbAddress: process.env.MONGO_URL || 'mongodb://admin:admin1234@ds063158.mlab.com:63158/node-react'
   },
 };
